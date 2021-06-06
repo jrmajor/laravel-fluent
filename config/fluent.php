0896318686d1f10747d652ca3745abb4efad83cd
Jeremiasz Major <jrh.mjr@gmail.com>
2021-06-06 15:34:27 +0200

    add comments to config file

--- config/fluent.php
+++ config/fluent.php
@@ -2,8 +2,19 @@
 
 return [
 
+    /*
+     * In strict mode, exceptions will be thrown for syntax errors
+     * in .ftl files, unknown variables in messages etc.
+     * It's recommended to enable this setting in development
+     * to make it easy to spot mistakes.
+     */
     'strict' => ! app()->isProduction(),
 
+    /*
+     * Determines if it should use Unicode isolation marks (FSI, PDI)
+     * for bidirectional interpolations. You may want to enable this
+     * behaviour if your application uses right-to-left script.
+     */
     'use_isolating' => false,
 
 ];
