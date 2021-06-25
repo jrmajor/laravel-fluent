d1983b2bb411584cf69d6797852f875ecdc332f0
Jeremiasz Major <jrh.mjr@gmail.com>
2021-06-25 17:35:06 +0200

    fix and simplify loader test

--- tests/LoaderTest.php
+++ tests/LoaderTest.php
@@ -1,30 +1,21 @@
 <?php
 
 use Illuminate\Filesystem\Filesystem;
-use Illuminate\Foundation\Application;
 use Illuminate\Translation\Translator as BaseTranslator;
 use Major\Fluent\Laravel\FluentTranslator;
 
 it('tries to load a non-existent file only once', function () {
-    $app = Application::getInstance();
-
-    $app->forgetInstance('translator');
-    $app->forgetExtenders('translator');
-
-    $app->extend(
-        'translator',
-        fn (BaseTranslator $service, Application $app) => new FluentTranslator(
-            baseTranslator: $service,
-            files: Mockery::mock(Filesystem::class)
-                ->shouldReceive('exists')->twice()->andReturn(false)
-                ->getMock(),
-            path: $app['path.lang'],
-            locale: 'pl', fallback: 'en',
-            bundleOptions: ['strict' => true, 'useIsolating' => false],
-        ),
+    $translator = new FluentTranslator(
+        baseTranslator: app(BaseTranslator::class),
+        files: Mockery::mock(Filesystem::class)
+            ->shouldReceive('exists')->twice()->andReturn(false)
+            ->getMock(),
+        path: app('path.lang'),
+        locale: 'pl', fallback: 'en',
+        bundleOptions: ['strict' => true, 'useIsolating' => false],
     );
 
-    __('missing.message');
+    $translator->get('missing.message');
 
-    __('missing.message');
+    $translator->get('missing.message');
 });
