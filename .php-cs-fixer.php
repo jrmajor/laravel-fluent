0496415d1d4b947068eda707f21024163355fe41
Jeremiasz Major <jrh.mjr@gmail.com>
2022-03-18 22:40:08 +0100

    move all caches to .cache

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -4,4 +4,5 @@ $finder = PhpCsFixer\Finder::create()
     ->in(__DIR__)
     ->ignoreVCS(true);
 
-return Major\CS\config($finder);
+return Major\CS\config($finder)
+    ->setCacheFile('.cache/.php-cs-fixer.cache');
