42288e42969156f388b4fdaf41545378a1d035ec
Jeremiasz Major <jrh.mjr@gmail.com>
2022-06-19 19:28:21 +0200

    fix php-cs-fixer configuration

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -2,7 +2,7 @@
 
 $finder = PhpCsFixer\Finder::create()
     ->in(__DIR__)
-    ->ignoreVCS(true);
+    ->ignoreVCSIgnored(true);
 
 return Major\CS\config($finder)
     ->setCacheFile('.cache/.php-cs-fixer.cache');
